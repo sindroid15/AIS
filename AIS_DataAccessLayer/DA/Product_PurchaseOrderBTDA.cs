cdfcf37649c55ba60c16fab23ad17e06d3e5ab5c
sindroid15 <sin.droid15@gmail.com>
2021-11-30 00:11:33 +0300

    Final version

---
+++ AIS_DataAccessLayer/DA/Product_PurchaseOrderBTDA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace AIS_DataAccessLayer.DA
+{
+    class Product_PurchaseOrderBTDA
+    {
+        AISEntitiesDB db;
+
+        public Product_PurchaseOrderBTDA()
+        {
+            db = new AISEntitiesDB();
+        }
+
+        public List<Product_PurchaseOrderBT> SelectAll()
+        {
+            return db.Product_PurchaseOrderBT.ToList();
+        }
+
+        public bool InsertProduct_PurchaseOrderBT(Product_PurchaseOrderBT productPurchaseBT)
+        {
+            db.Product_PurchaseOrderBT.Add(productPurchaseBT);
+            return db.SaveChanges() > 0 ? true : false;
+        }
+
+        
+    }
+}
